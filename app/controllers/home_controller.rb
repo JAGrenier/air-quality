66efad3ad826193b9f33cead81c948a019b8d488
JAGrenier <jenniferagrenier@gmail.com>
2021-06-18 15:43:46 -0600

    api call and display

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,19 @@
 class HomeController < ApplicationController
   def index
+    require 'net/http'
+    require 'json'
+    @url = 'https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=80226&distance=0&API_KEY=D9063C5B-5536-4936-B684-DF396C3E2295'
+    @uri = URI(@url)
+    @response = Net::HTTP.get(@uri)
+    @output = JSON.parse(@response)
+
+    if @output.empty? 
+      @final_output = "Error"
+    elsif !@output 
+      @final_output = "Error"
+    else
+      @final_output = @output[0]['AQI']
+    end
+    
   end
 end
