b92ba9588981d33b2ce7c8fc92c4a4e1efc351fc
JAGrenier <jenniferagrenier@gmail.com>
2021-06-19 18:41:18 -0600

    input for zipcode

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,10 @@ class HomeController < ApplicationController
   def index
     require 'net/http'
     require 'json'
-    @url = 'https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=80226&distance=0&API_KEY=D9063C5B-5536-4936-B684-DF396C3E2295'
+
+    @zip_code = params['zip_code'] || 80226
+
+    @url = "https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=#{@zip_code}&distance=0&API_KEY=D9063C5B-5536-4936-B684-DF396C3E2295"
     @uri = URI(@url)
     @response = Net::HTTP.get(@uri)
     @output = JSON.parse(@response)
