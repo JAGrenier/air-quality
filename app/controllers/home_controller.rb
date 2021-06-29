7436224fdb82f71bb7adc531c836d3995b0d5f5c
JAGrenier <jenniferagrenier@gmail.com>
2021-06-28 19:54:19 -0600

    More styling and added descriptions

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,6 +9,7 @@ class HomeController < ApplicationController
     @uri = URI(@url)
     @response = Net::HTTP.get(@uri)
     @output = JSON.parse(@response)
+    @reporting_area = @output[0]['ReportingArea']
 
     if @output.empty? 
       @final_output = 'Error'
@@ -20,18 +21,25 @@ class HomeController < ApplicationController
 
     if @final_output <= 50 
       @api_color = 'bg-green-300'
+      @description = 'Good: Air quality is satisfactory, and air pollution poses little or no risk.'
     elsif @final_output >= 51 && @final_output <= 100 
       @api_color = 'bg-yellow-300'
+      @description = 'Moderate: Air quality is acceptable. However, there may be a risk for some people, particularly those who are unusually sensitive to air pollution.'
     elsif @final_output >= 101 && @final_output <= 150
       @api_color = 'bg-orange-300'
+      @description = 'Unhealthy for Sensitive Groups: Members of sensitive groups may experience health effects. The general public is less likely to be affected.'
     elsif @final_output >= 151 && @final_output <= 200 
       @api_color = 'bg-red-300'
+      @description = 'Unhealthy: Some members of the general public may experience health effects; members of sensitive groups may experience more serious health effects.'
     elsif @final_output >= 201 && @final_output <= 300 
       @api_color = 'bg-purple-300'
+      @description = 'Very Unhealthy: Health alert: The risk of health effects is increased for everyone.'
     elsif @final_output >= 301 && @final_output <= 500 
       @api_color = 'bg-rose-300'
+      @description = 'Hazardous: Health warning of emergency conditions: everyone is more likely to be affected.'
     else  
       @api_color = 'bg-gray-300'
+      @description = 'Error'
     end
     
   end
